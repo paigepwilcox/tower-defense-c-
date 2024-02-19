07392c739f1244a1955b8bbfdbbf91dc7f209bd0
Paige <ph.paigee@gmail.com>
2024-02-19 16:06:00 -0500

    corrected the Main function to include static keyword, instatiated the Map class and tested correct output with dotnet run

--- Game.cs
+++ Game.cs
@@ -2,10 +2,15 @@ namespace TowerDefenseGame
 {
     class Game
     {
-        public void Main()
+        public static void Main()
         {
-            // Instatiation - an instance of a class. creates a new variable tower with the type Tower, assigning to a new Tower() object. 
+            // Instatiation - an instance of a class. creates a new variable tower with the type Tower, assigning tower to a new Tower() object. 
             Tower tower = new Tower();
+
+            Map map = new Map(8, 5);
+
+            int area = map.Width * map.Height;
+            Console.WriteLine(area);
         }
     }
 }
