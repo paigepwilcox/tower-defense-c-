5f0d43106c5e3dc20aaba20990c8f686b123961f
Paige <ph.paigee@gmail.com>
2024-02-19 16:06:45 -0500

    created a constructor method that has the attributs that will create the map or game board

--- Map.cs
+++ Map.cs
@@ -1,7 +1,17 @@
+// readonly modifier means that the values cannot be changed
+
 namespace TowerDefenseGame
 {
     class Map 
     {
-        
+        public readonly int Width;
+        public readonly int Height;
+
+        // constructor method - never takes void or type
+        public Map(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
     }
 }
