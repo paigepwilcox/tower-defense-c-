a6273db4aedb83303315c3be50baee052538dad0
Paige <ph.paigee@gmail.com>
2024-02-19 11:34:14 -0500

    created file structure

---
+++ Game.cs
@@ -0,0 +1,11 @@
+namespace TowerDefenseGame
+{
+    class Game
+    {
+        public void Main()
+        {
+            // Instatiation - an instance of a class. creates a new variable tower with the type Tower, assigning to a new Tower() object. 
+            Tower tower = new Tower();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # C# Tower Defense
 
 In preperation for a Microsoft intership role that requires C# knoweldge I made a tower defense game. I exemplify begginer knowledge in C#, while also exercising OOP concepts.
+
+
+My Objects 
