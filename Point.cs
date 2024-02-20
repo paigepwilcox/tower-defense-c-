c7ee9c3806eae3ebc287b7c67673bd884e8a1471
Paige <ph.paigee@gmail.com>
2024-02-20 13:25:41 -0500

    created Point class with x,y attributes

---
+++ Point.cs
@@ -0,0 +1,11 @@
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace TowerDefenseGame
+{
+    class Point 
+    {
+        public int X; 
+        public int Y;
+    } 
+}
