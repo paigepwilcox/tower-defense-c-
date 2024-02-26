9feab3779770cddeb8407f50f9977bac3f87093d
Paige <ph.paigee@gmail.com>
2024-02-26 10:55:41 -0500

    write and tested a try and catch to handle errors relating to the mao

--- Game.cs
+++ Game.cs
@@ -5,9 +5,19 @@ namespace TowerDefenseGame
         public static void Main()
         {
             // Instatiation - an instance of a class. creates a new variable tower with the type Tower, assigning tower to a new Tower() object. 
-            Tower tower = new Tower();
+            /// Tower tower = new Tower();
 
             Map map = new Map(8, 5);
+            try
+            {
+            MapLocation mapLocation = new MapLocation(20, 20, map);
+            }
+            catch(Exception ex)
+            {
+                // throw new System.Exception(ex.Message);
+                Console.WriteLine(ex.Message);
+            }
+
 
             int area = map.Width * map.Height;
             Console.WriteLine(area);
