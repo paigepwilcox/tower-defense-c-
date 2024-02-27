4522c479e07c14461a8476bee16c09f1103bb670
Paige <ph.paigee@gmail.com>
2024-02-27 15:20:45 -0500

    wrote a location field, created a constructor that assigns location a value, started FireOnInvaders method and wrote my first c# for loop

--- Tower.cs
+++ Tower.cs
@@ -2,6 +2,23 @@ namespace TowerDefenseGame
 {
     class Tower 
     {
-        
+        private readonly MapLocation _location;
+
+        public Tower(MapLocation location)
+        {
+            _location = location;
+            // if (location.OnPath(_location))
+            // {
+            //     throw new OutOfBoundsException(location + "towers cannot be on the path");
+            // }
+        }
+
+        public void FireOnInvaders(Invader[] invaders)
+        {
+            for(int i = 0; i < invaders.Length; i++)
+            {
+                Invader invader = invaders[i]
+            }
+        }
     }
 }
