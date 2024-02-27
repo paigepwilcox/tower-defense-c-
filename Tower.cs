bea7a1b4905d60219e956d5fc9dd9217eeae97fb
Paige <ph.paigee@gmail.com>
2024-02-27 16:23:57 -0500

    refactored the for loop into a for each loop, FIX THE CONSTRUCTOR

--- Tower.cs
+++ Tower.cs
@@ -2,6 +2,8 @@ namespace TowerDefenseGame
 {
     class Tower 
     {
+        private const int _range = 1;
+        private const int _power = 1;
         private readonly MapLocation _location;
 
         public Tower(MapLocation location)
@@ -15,9 +17,13 @@ namespace TowerDefenseGame
 
         public void FireOnInvaders(Invader[] invaders)
         {
-            for(int i = 0; i < invaders.Length; i++)
+            foreach(Invader invader in invaders)
             {
-                Invader invader = invaders[i]
+                if(invader.IsActive && _location.InRangeOf(invader.Location, _range))
+                {
+                    invader.DecreaseHealth(_power);
+                    break;
+                }
             }
         }
     }
