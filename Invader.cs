dd7e5c8e2fbb23e5078b70b0d24bad839ba3b5c8
Paige <ph.paigee@gmail.com>
2024-02-27 15:01:47 -0500

    wrote IsNeutralized property to determine if an invaders health has run out, IsActive property is written to determine if an invader is still active

--- Invader.cs
+++ Invader.cs
@@ -12,6 +12,12 @@ namespace TowerDefenseGame
 
         // private set to let coders know to use the method
         public int Health { get; private set; } = 2;
+        public bool IsNeutralized => Health <= 0;
+
+        public bool IsActive => !(IsNeutralized || HasScored);
+
+        // see if the invader has scored by using the Path and _pathstep fields
+        public bool HasScored { get { return _pathStep >- _path.Length; } }
         
         // constructor makes sure our fields are not null
         public Invader(Path path)
