1d675c5ab47376095d5317b2b8b70f7ec123acef
Paige <ph.paigee@gmail.com>
2024-02-27 10:13:52 -0500

    creating getter and setter functions relating to the location of the invader

--- Invader.cs
+++ Invader.cs
@@ -2,6 +2,20 @@ namespace TowerDefenseGame
 {
     class Invader 
     {
-        
+        // instatianting a MapLocation class called _location - named this way bc _location is a private field.
+        private MapLocation _location;
+
+        //getter
+        public MapLocation GetLocation()
+        {
+            return _location;
+        }
+
+        //setter
+        public void SetLocation(MapLocation value)
+        {
+            _location = value;
+        }
+
     }
 }
