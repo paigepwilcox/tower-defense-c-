4fb98590cd544fbd727839b3952f73d0da6a8c51
Paige <ph.paigee@gmail.com>
2024-02-27 16:51:55 -0500

    created uncertainity by using random to decide the accuracy of shot, everything is working but my invader seems to be always out of range or inactive, time to debug

--- Tower.cs
+++ Tower.cs
@@ -1,9 +1,15 @@
+using System.Runtime.InteropServices;
+
 namespace TowerDefenseGame
 {
     class Tower 
     {
         private const int _range = 1;
         private const int _power = 1;
+        private const double _accuracy = .75;
+
+        private static readonly System.Random _random = new System.Random();
+
         private readonly MapLocation _location;
 
         public Tower(MapLocation location)
@@ -15,13 +21,33 @@ namespace TowerDefenseGame
             // }
         }
 
+        public bool IsSuccessfulShot()
+        {
+            return _random.NextDouble() < _accuracy;
+        }
+
         public void FireOnInvaders(Invader[] invaders)
         {
             foreach(Invader invader in invaders)
             {
+                Console.WriteLine("made it into the foreach loop");
                 if(invader.IsActive && _location.InRangeOf(invader.Location, _range))
                 {
-                    invader.DecreaseHealth(_power);
+                    Console.WriteLine("passed first condition");
+                    if(IsSuccessfulShot())
+                    {
+                        invader.DecreaseHealth(_power);
+                        Console.WriteLine("shots fired!");
+
+                        if(invader.IsNeutralized)
+                        {
+                            Console.WriteLine("enemy neutralized!");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("shots missed!");
+                    }
                     break;
                 }
             }
