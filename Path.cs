0fab78e932ab1c42702d6b1b1c210ba9a65f9ecb
Paige <ph.paigee@gmail.com>
2024-02-27 14:59:32 -0500

    created a length property that will find the length of the encapsulated path array

--- Path.cs
+++ Path.cs
@@ -4,10 +4,14 @@ namespace TowerDefenseGame
     {
         private readonly MapLocation[] _path;
 
+        // returns the length of the path array that is encapsalated in the path glass
+        public int Length => _path.Length;
+
         public Path(MapLocation[] path)
         {
             _path = path;
         }
+
         public MapLocation GetLocationAt(int pathStep)
         {
             return (pathStep < _path.Length) ? _path[pathStep] : null;
