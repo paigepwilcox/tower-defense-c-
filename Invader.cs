2b87963c5e7a5d34dbde945d4ce6d325827e5c47
Paige <ph.paigee@gmail.com>
2024-02-27 14:32:06 -0500

    drying this code by way of computed properties

--- Invader.cs
+++ Invader.cs
@@ -5,23 +5,29 @@ namespace TowerDefenseGame
         // storing an instance of the path object so our Move() function can know and set the location of the invader
         private readonly Path _path;
         private int _pathStep = 0;
+        
         // writing properites
-        public MapLocation Location { get; private set; }
+        // computed property - a property that does not wrap an actual field
+        public MapLocation Location 
+        { 
+            get
+            {
+                return _path.GetLocationAt(_pathStep);
+            }
+        }
 
         // constructor makes sure our fields are not null
         public Invader(Path path)
         {
             _path = path;
-            Location = path.GetLocationAt(0);
         }
 
         // to advance to invader
         public void Move()
         {
             _pathStep += 1;
-            Location = _path.GetLocationAt(_pathStep);
-            
         }
 
     }
 }
+
