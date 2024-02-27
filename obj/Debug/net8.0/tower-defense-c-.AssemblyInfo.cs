5bb2a282e87878d797fc64907b30ab25707c33c9
Paige <ph.paigee@gmail.com>
2024-02-27 16:52:12 -0500

    dependencies

--- obj/Debug/net8.0/tower-defense-c-.AssemblyInfo.cs
+++ obj/Debug/net8.0/tower-defense-c-.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tower-defense-c-")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ee4a97fb6ebc165b0a543f3e448cf03c0a089d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+711031caf0665014b8c9c2183cf44492dfcc25e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("tower-defense-c-")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tower-defense-c-")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
