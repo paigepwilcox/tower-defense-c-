a04d08ef1f3a0a886ab81b721f4cee086091637f
Paige <ph.paigee@gmail.com>
2024-02-27 10:07:49 -0500

    practice encapsulation with Path and MapLocation, created a function that returns the point at index pathStep, practiced using ternary statements

--- Path.cs
+++ Path.cs
@@ -2,6 +2,15 @@ namespace TowerDefenseGame
 {
     class Path 
     {
-        
+        private readonly MapLocation[] _path;
+
+        public Path(MapLocation[] path)
+        {
+            _path = path;
+        }
+        public MapLocation GetLocationAt(int pathStep)
+        {
+            return (pathStep < _path.Length) ? _path[pathStep] : null;
+        }
     }
 }
